bfbf44b244fdd24d5303ad8334d03927c801a2e9
Aparupa Bhattacharyya <40363978+anonymous-platypus@users.noreply.github.com>
2018-06-27 01:07:41 +0530

    Update UserController.java

--- src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
+++ src/main/java/com/thinkxfactor/zomatoplus/controller/UserController.java
@@ -1,22 +1,43 @@
 package com.thinkxfactor.zomatoplus.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkxfactor.zomatoplus.models.User;
+import com.thinkxfactor.zomatoplus.repository.UserRepository;
+
+import java.util.*;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-
 	
-
-	@GetMapping("/login")
-	public String userLogin() {
-		return "Hello from thinkxfactor";
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/login")
+	public User userLogin(@RequestParam("Name") String name,@RequestParam("Password") String password) {
+		User persistedUser=userRepository.findByNameAndPassword(name, password);
+		return persistedUser;
 	}
-
 	
-
+	@GetMapping("/getAll")
+	public List<User> getAll() {
+		List <User> listofusers=userRepository.findAll();
+		return listofusers;
+	}
+	
+	@PostMapping("/add")
+	public User addUser(@RequestBody User user) {
+		User persistedUser=userRepository.save(user);
+		return persistedUser;
+	}
+	
 }
