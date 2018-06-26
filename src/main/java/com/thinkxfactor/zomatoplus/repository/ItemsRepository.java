47dda4e688bd9b07221a9735c1ea1aa5d2162885
Aparupa Bhattacharyya <40363978+anonymous-platypus@users.noreply.github.com>
2018-06-27 01:08:31 +0530

    Add files via upload

---
+++ src/main/java/com/thinkxfactor/zomatoplus/repository/ItemsRepository.java
@@ -0,0 +1,10 @@
+package com.thinkxfactor.zomatoplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thinkxfactor.zomatoplus.models.Items;
+
+public interface ItemsRepository extends JpaRepository<Items, Long> {
+	
+	Items findByNameAndPrice(String name,Double price);
+}
