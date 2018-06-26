72223c79168632dd6dbcc5f397d0733c748cd772
Aparupa Bhattacharyya <40363978+anonymous-platypus@users.noreply.github.com>
2018-06-27 01:06:15 +0530

    Add files via upload

---
+++ src/main/java/com/thinkxfactor/zomatoplus/controller/RestaurantController.java
@@ -0,0 +1,49 @@
+package com.thinkxfactor.zomatoplus.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkxfactor.zomatoplus.models.Restaurant;
+import com.thinkxfactor.zomatoplus.repository.RestaurantRepository;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+	
+
+	
+
+	
+	@Autowired
+	private RestaurantRepository restaurantRepository;
+	
+	
+	@GetMapping("/getAll")
+	public List<Restaurant> getAll() {
+		List <Restaurant> listofrestaurants=restaurantRepository.findAll();
+		return listofrestaurants;
+	}
+	
+	@PostMapping("/add")
+	public Restaurant addRestaurant(@RequestBody Restaurant restaurant) {
+		Restaurant persistedRestaurant=restaurantRepository.save(restaurant);
+		return persistedRestaurant;
+	}
+	
+	@PostMapping("/search")
+	public Restaurant searchRestaurant(@RequestParam("Name") String name,@RequestParam("Address") String address)
+	{
+		Restaurant persistedRestaurant=restaurantRepository.findByNameAndAddress(name, address);
+		return persistedRestaurant;
+	}
+	
+	
+}
